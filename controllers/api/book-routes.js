1e75fe71619b2e341b06ca02145fa922085e1bbd
aaron <afarrell002@gmail.com>
2021-12-17 14:24:12 -0700

    added functionality to the api books routes for get and get by id

---
+++ controllers/api/book-routes.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const { Books } = require("../../models");
+
+router.get("/", async (req, res) => {
+  try {
+    const books = await Books.findAll();
+    res.status(200).json(books);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const books = await Books.findByPk(req.params.id);
+
+    if (!books) {
+      res.status(404).json({
+        message: "No books found with that ID!",
+      });
+      return;
+    }
+    res.status(200).json(books);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
