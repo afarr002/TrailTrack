2f510818253de34faa60353d72863a781d184ca0
aaron <afarrell002@gmail.com>
2021-12-28 14:12:42 -0700

    started futureweatherapiurl call

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const insertCampgroundsEl = document.querySelector("#insertCampgroundsEl");
 
 const getCampgrounds = () => {
-  const getCampApiUrl = `https://developer.nps.gov/api/v1/campgrounds?stateCode=CO&limit=25&api_key=oSfzNz3dTkFvHFrco2C2sYljO4uhXBaTYHwx7kib`;
+  const getCampApiUrl = `https://developer.nps.gov/api/v1/campgrounds?stateCode=CO&api_key=oSfzNz3dTkFvHFrco2C2sYljO4uhXBaTYHwx7kib`;
 
   fetch(getCampApiUrl)
     .then((res) => {
@@ -12,46 +12,45 @@ const getCampgrounds = () => {
       // futureWeather();
 
       for (i = 0; i < info.data.length; i++) {
-        const weatherApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${info.data[2].latitude}&lon=${info.data[2].longitude}&appid=66cae1bd334eb7747100576e02048328&units=imperial`;
-
-        fetch(weatherApiUrl)
-          .then((res) => {
-            return res.json();
-          })
-          .then((weatherInfo) => {
-            console.log(weatherInfo);
-            const template = `<div class="card" style="width: 18rem">
-      <div class="card-header whiteText">${info.data[2].name}</div>
-        <ul class="list-group list-group-flush">
-          <li class="list-group-item">Description: ${
-            info.data[2].description
-          }</li>
-          <li class="list-group-item">Fees: ${
-            info.data[2].fees.length > 0 ? info.data[2].fees[0].cost : ""
-          }</li>
-          <li class="list-group-item">URL: <a href="${
-            info.data[2].url
-          }">Click here to visit ${
-              info.data[2].name
-            }'s National Park Service Page</a></li>
-          <li class="list-group-item">Images: <img src="${
-            info.data[2].images.length > 0 ? info.data[2].images[0].url : ""
-          }" alt="${
-              info.data[2].images.length > 0
-                ? info.data[2].images[0].altText
-                : ""
-            }"></li>
-          <li class="list-group-item"></li>
-        </ul>
-  </div>`;
-            insertCampgroundsEl.innerHTML = template;
-          });
+        const template = `<div class="card" style="width: 18rem">
+            <div class="card-header whiteText">${info.data[i].name}</div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">Description: ${
+                  info.data[i].description
+                }</li>
+                <li class="list-group-item">Fees: ${
+                  info.data[i].fees.length > 0 ? info.data[i].fees[0].cost : ""
+                }</li>
+                <li class="list-group-item">URL: <a href="${
+                  info.data[i].url
+                }">Click here to visit ${
+          info.data[i].name
+        }'s National Park Service Page</a></li>
+                <li class="list-group-item">Images: <img src="${
+                  info.data[i].images.length > 0
+                    ? info.data[i].images[0].url
+                    : ""
+                }" alt="${
+          info.data[i].images.length > 0 ? info.data[i].images[0].altText : ""
+        }"></li>
+                <li class="list-group-item"></li>
+              </ul>
+        </div>`;
+        insertCampgroundsEl.innerHTML = template;
       }
     });
 };
 
-const futureWeather = () => {
-  for (i = 0; i > weatherInfo.daily.length; i++) {}
+const futureWeather = (info) => {
+  const weatherApiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${info.data[2].latitude}&lon=${info.data[2].longitude}&appid=66cae1bd334eb7747100576e02048328&units=imperial`;
+
+  fetch(weatherApiUrl)
+    .then((res) => {
+      return res.json();
+    })
+    .then((weatherInfo) => {
+      console.log(weatherInfo);
+    });
 };
 
 getCampgrounds();
