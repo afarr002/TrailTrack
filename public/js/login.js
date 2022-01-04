25ddb3ffe82a2e3d7cacae49196b28f26ad30eab
Charlie Highum <chighum41@gmail.com>
2022-01-03 19:59:02 -0700

    fixed functionality for user signup

--- public/js/login.js
+++ public/js/login.js
@@ -12,7 +12,7 @@ const loginFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace("/");
+      document.location.replace("/home");
     } else {
       alert("Failed to log in.");
     }
@@ -22,9 +22,9 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
   event.preventDefault();
 
-  const username = document.querySelector("#username").value.trim();
-  const email = document.querySelector("#email-login").value.trim();
-  const password = document.querySelector("#password-login").value.trim();
+  const username = document.querySelector("#username-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
 
   console.log(username);
   console.log(email);
@@ -38,7 +38,7 @@ const signupFormHandler = async (event) => {
     });
 
     if (response.ok) {
-      document.location.replace("/");
+      document.location.replace("/home");
     } else {
       alert("Failed to sign up.");
     }
